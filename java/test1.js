d47357e4223774be7212d7609904bd3d6c02ed5d
JeChicla <162261345+JeChicla@users.noreply.github.com>
2024-09-09 15:12:16 -0300

    Create test1.js

---
+++ java/test1.js
@@ -0,0 +1,21 @@
+let resultado;
+let x = 3;
+
+if (x < 40) {
+  resultado = "livro é barato.";
+} else {
+  resultado = "livro nao é barato"
+}
+
+console.log(resultado)
+console.log(x === 3);
+
+let meuObjeto = {
+  nome : 'julia siodaro',
+  idade : 18
+}
+console.log(meuObjeto)
+
+const seta = (entrada)=> {
+  return entrada + 33;
+}
