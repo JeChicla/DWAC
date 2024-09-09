0bd9cba643b18c8406f410a1b78b7a66107060f3
JeChicla <162261345+JeChicla@users.noreply.github.com>
2024-09-09 16:48:30 -0300

    Update test1.js

--- java/test1.js
+++ java/test1.js
@@ -19,3 +19,19 @@ console.log(meuObjeto)
 const seta = (entrada)=> {
   return entrada + 33;
 }
+const array = [21,34, 'Je',[32, 52]]
+
+function func2(entrada){
+  let r;
+  let resultado
+  if (entrada % 2 === 0){
+    r = 'par';
+  } else{
+    r = 'impar';
+  }
+  return r
+  
+}
+array.forEach((el) => console.log(el))
+
+const funcimprime = (ele,indice) => console.log("indice",indice, ":", ele)
